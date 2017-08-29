991a90346e4160c0bc051fc59da58a8434effe81
Islane <islane.junior@gmail.com>
2017-08-29 10:35:01 -0300

    Adição da activity Publications, setando orientação, setado label

---
+++ app/src/main/java/harmonytech/praagoraa/view/PublicationsActivity.java
@@ -0,0 +1,15 @@
+package harmonytech.praagoraa.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import harmonytech.praagoraa.R;
+
+public class PublicationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publications);
+    }
+}
